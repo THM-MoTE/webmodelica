a6755d3baaa3e08a8eec77a83eee015a789b4dd8
N. Justus <nicola.justus@mni.thm.de>
2019-02-13 16:19:40 +0100

    open given file in MonacoEditor

--- ui/src/components/editor.tsx
+++ ui/src/components/editor.tsx
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import * as monaco from 'monaco-editor';
 import { Container } from '../layouts'
+import { File } from '../models/file'
 
-export class CodeEditor extends Component {
+export class CodeEditor extends Component<any, any> {
 
-  private file: string
-  private containerName: string
   private monacoEditor: any = undefined
+  private containerName: string
+  private file: File
 
   constructor(props: any) {
     super(props)
-    this.file = "example.mo"
-    this.containerName = `editor-${this.file}`
+    this.file = this.props.file
+    this.containerName = `editor-${this.file.relativePath}`
   }
 
   componentDidMount() {
     if (!this.monacoEditor) {
       this.monacoEditor = monaco.editor.create(document.getElementById(this.containerName) as HTMLElement, {
-        value: "// First line\nfunction hello() {\n\talert('Hello world!');\n}\n// Last line",
+        // value: "// First line\nfunction hello() {\n\talert('Hello world!');\n}\n// Last line",
+        value: this.file.content,
         language: "typescript"
       })
     }
@@ -25,9 +27,7 @@ export class CodeEditor extends Component {
 
   render() {
     return (
-      <Container>
-        <div id={this.containerName} className="editor"></div>
-      </Container>
+      <div id={this.containerName} className="editor"></div>
     )
   }
 }
--- ui/src/components/editors-pane.tsx
+++ ui/src/components/editors-pane.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Col } from 'react-bootstrap'
+import { CodeEditor } from './editor'
 
 export class EditorsPane extends React.Component<any, any> {
   constructor(props: any) {
     super(props)
+    console.log("EditorsPane: opened files", this.props.files)
   }
 
   render() {
-    return (<Col>blup editors</Col>)
+    const files = this.props.files
+    return (
+      <Col>
+        {files.length > 0 &&
+          <CodeEditor file={files[0]} />
+        }
+      </Col>
+    )
   }
 }
