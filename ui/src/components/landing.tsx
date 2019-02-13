6b77338ac84f5a461cb246deb359e298ef4a53e8
N. Justus <nicola.justus@mni.thm.de>
2019-02-13 14:14:36 +0100

    after login, redirect to projects-view

--- ui/src/components/landing.tsx
+++ ui/src/components/landing.tsx
@@ -12,47 +12,40 @@ export class Landing extends Component<any,any> {
   constructor(props:any) {
     super(props)
     this.api = props.api
-    this.state = {loggedIn: false}
   }
 
   private handleSubmit() {
+    const props = this.props
     console.log("name", this.username, "pw", this.password)
     this.api.login(this.username, this.password)
       .then(res => console.log("response:", res))
-      .then(() => this.setState({loggedIn: true}))
+      .then(() => props.history.push('/projects') )
   }
 
   render() {
     const usernameChanged = (ev:any) => this.username = ev.target.value
     const passwordChanged = (ev:any) => this.password = ev.target.value
-    if(this.state.loggedIn) {
-      return (<Redirect to='/project-view' />)
-    }
-    else {
-      return (
-          <Container>
-          <div className="row align-items-center">
-            <div className="card sm-10 mx-auto">
-              <h5 className="card-title">Login</h5>
-              <div className="card-body">
-                  <Form>
-                    <Form.Group controlId="formUsername">
-                      <Form.Label>Username</Form.Label>
-                      <Form.Control placeholder="Enter username" onChange={usernameChanged}/>
-                    </Form.Group>
-                    <Form.Group controlId="formPassword">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" placeholder="Password" onChange={passwordChanged}/>
-                    </Form.Group>
-                    <Button variant="primary" onClick={this.handleSubmit.bind(this)}>
-                      Submit
-                    </Button>
-                  </Form>
-                </div>
+    return (<Container>
+      <div className="row align-items-center">
+        <div className="card sm-10 mx-auto">
+          <h5 className="card-title">Login</h5>
+          <div className="card-body">
+              <Form>
+                <Form.Group controlId="formUsername">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control placeholder="Enter username" onChange={usernameChanged}/>
+                </Form.Group>
+                <Form.Group controlId="formPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" onChange={passwordChanged}/>
+                </Form.Group>
+                <Button variant="primary" onClick={this.handleSubmit.bind(this)}>
+                  Submit
+                </Button>
+              </Form>
             </div>
-          </div>
-        </Container>
-      )
-    }
+        </div>
+      </div>
+    </Container>)
   }
 }
