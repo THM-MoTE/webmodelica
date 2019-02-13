c86c1bb05e092860b1c727574254c2b83e6bd69d
N. Justus <nicola.justus@mni.thm.de>
2019-02-13 11:42:42 +0100

    handle login pressed

--- ui/src/components/landing.tsx
+++ ui/src/components/landing.tsx
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import {Container} from '../layouts'
+import {Button, Form} from 'react-bootstrap'
 
 export class Landing extends Component {
+  private username:string = ''
+  private password:string = ''
+
+  constructor(props:any) {
+    super(props)
+  }
+
+  private handleSubmit() {
+    console.log("name", this.username, "pw", this.password)
+  }
+
   render() {
+    const usernameChanged = (ev:any) => this.username = ev.target.value
+    const passwordChanged = (ev:any) => this.password = ev.target.value
+
     return (
         <Container>
         <div className="row align-items-center">
           <div className="card sm-10 mx-auto">
             <h5 className="card-title">Login</h5>
             <div className="card-body">
-              <form>
-                <div className="form-group">
-                  <label>Username</label>
-                  <input className="form-control" id="username" aria-describedby="usernameHelp" placeholder="Enter username" />
-                </div>
-                <div className="form-group">
-                  <label>Password</label>
-                  <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                </div>
-                <button type="submit" className="btn btn-primary">Login</button>
-                </form>
+                <Form>
+                  <Form.Group controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control placeholder="Enter username" onChange={usernameChanged}/>
+                  </Form.Group>
+                  <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" onChange={passwordChanged}/>
+                  </Form.Group>
+                  <Button variant="primary" onClick={this.handleSubmit.bind(this)}>
+                    Submit
+                  </Button>
+                </Form>
               </div>
           </div>
         </div>
