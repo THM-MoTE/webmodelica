b38dbeef3fcff8d36e39105ad50bced1032d8dfe
N. Justus <nicola.justus@mni.thm.de>
2019-02-14 15:29:37 +0100

    extract authentication from AppState

--- ui/src/redux/index.ts
+++ ui/src/redux/index.ts
@@ -1,7 +1,14 @@
 import {Action} from './actions'
+import {AppState} from '../models/state'
 
 export function defaultMapDispatchToProps(dispatch: (a:Action) => void) {
   return {
     dispatch: dispatch
   };
+}
+
+export function mapAuthenticationToProps(state:AppState) {
+  return {
+    authentication: state.authentication
+  }
 }
