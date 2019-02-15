6aec88e7504736468bae6015e6a49e5526bcf1a8
N. Justus <nicola.justus@mni.thm.de>
2019-02-15 13:52:11 +0100

    provide a form for adding new projects

--- ui/src/components/project-view.tsx
+++ ui/src/components/project-view.tsx
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import { AppState, Project } from '../models/index'
-import {Action, setProjects} from '../redux/index'
+import { Action, setProjects, addProject } from '../redux/index'
 import { Container } from '../layouts'
 import { ApiClient, defaultClient } from '../services/api-client'
-import { ListGroup, Card, Button } from 'react-bootstrap'
-import {connect} from 'react-redux'
+import { ListGroup, Card, Form, Button, Col } from 'react-bootstrap'
+import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as R from 'ramda'
 
 class ProjectViewCon extends Component<any, any> {
   private api: ApiClient
+  private newProjectName: string
 
   constructor(props: any) {
     super(props)
     this.api = defaultClient
+    this.newProjectName = ''
   }
 
   public componentDidMount() {
     this.api.projects()
-      .then(ps => {
-        this.props.setProjects(ps)
-      })
+      .then(this.props.setProjects)
   }
 
   private newSession(ev: any, p: Project): void {
@@ -30,33 +30,39 @@ class ProjectViewCon extends Component<any, any> {
     ev.preventDefault()
   }
 
-  public render() {
+  private newProject() {
+    this.api.newProject(this.props.username, this.newProjectName)
+      .then(this.props.addProject)
+  }
 
-    const click = () => {
-      let rnd = Math.floor((Math.random() * 10) + 1)
-      this.props.setProjects([{ id: "12"+rnd, name: "Project "+rnd, owner: "Nico" }])
-    }
+  public render() {
+    const newProjectNameChanged = (ev: any) => this.newProjectName = ev.target.value
 
     return (<Container>
       <Card>
         <Card.Header>Your Projects</Card.Header>
         <ListGroup variant="flush">
+          <ListGroup.Item>
+            <Form.Row className="justify-content-md-center">
+              <Col sm={10}><Form.Control placeholder="Enter project name" onChange={newProjectNameChanged} /></Col>
+              <Col sm={1}><Button variant="outline-primary" onClick={this.newProject.bind(this)}>New Project</Button></Col>
+            </Form.Row>
+          </ListGroup.Item>
           {
             this.props.projects && this.props.projects.map((p: Project) =>
               (<ListGroup.Item action href={"#" + p.id} onClick={(ev: any) => this.newSession(ev, p)} key={p.id}>{p.owner} - {p.name}</ListGroup.Item>))
           }
         </ListGroup>
-        <Button onClick={click}>test</Button>
       </Card>
-    </Container>)
+    </Container >)
   }
 }
 
-function mapToProps(state:AppState) {
-  return { projects: state.projects }
+function mapToProps(state: AppState) {
+  return { projects: state.projects, username: state.authentication!.username }
 }
-function dispatchToProps(dispatch: (a:Action) => any) {
-  return bindActionCreators({setProjects}, dispatch)
+function dispatchToProps(dispatch: (a: Action) => any) {
+  return bindActionCreators({ setProjects, addProject }, dispatch)
 }
 const ProjectView = connect(mapToProps, dispatchToProps)(ProjectViewCon)
 export default ProjectView;
