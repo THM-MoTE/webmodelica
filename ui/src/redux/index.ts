f844968a112526d8fdfb8ddd5cd98397e6da90ae
N. Justus <nicola.justus@mni.thm.de>
2019-02-15 13:52:00 +0100

    format index.ts :shrug:

--- ui/src/redux/index.ts
+++ ui/src/redux/index.ts
@@ -1,15 +1,15 @@
 export * from './actions'
 export * from './reducers'
-import {Action} from './actions'
-import {AppState} from '../models/state'
+import { Action } from './actions'
+import { AppState } from '../models/state'
 
-export function defaultMapDispatchToProps(dispatch: (a:Action) => void) {
+export function defaultMapDispatchToProps(dispatch: (a: Action) => void) {
   return {
     dispatch: dispatch
   };
 }
 
-export function mapAuthenticationToProps(state:AppState) {
+export function mapAuthenticationToProps(state: AppState) {
   return {
     authentication: state.authentication
   }
