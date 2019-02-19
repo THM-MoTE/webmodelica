b07a1d5f36d0c5a38901ba28276986da438fe8be
N. Justus <nicola.justus@mni.thm.de>
2019-02-19 15:26:31 +0100

    define custom serializer for nio.Path's #20

--- src/main/scala/webmodelica/core/JsonConfigModule.scala
+++ src/main/scala/webmodelica/core/JsonConfigModule.scala
@@ -1,8 +1,24 @@
 package webmodelica.core
 
+import java.nio.file.Path
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import com.twitter.finatra.json.modules._
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.{JsonSerializer, PropertyNamingStrategy, SerializerProvider}
 
 object JsonConfigModule extends FinatraJacksonModule {
   override val propertyNamingStrategy = PropertyNamingStrategy.LOWER_CAMEL_CASE
+
+  val pathSerializer = new JsonSerializer[Path] {
+    override def handledType(): Class[Path] = classOf[Path]
+    override def serialize(value: Path, gen: JsonGenerator, serializers: SerializerProvider): Unit = {
+      gen.writeString(value.toString)
+    }
+  }
+
+  override val additionalJacksonModules: Seq[SimpleModule] = Seq(
+    new SimpleModule { addSerializer(pathSerializer) }
+  )
 }
