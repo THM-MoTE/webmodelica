5b633758b6a6a81f2e38c75e7d3e2372e50a5f5e
N. Justus <nicola.justus@mni.thm.de>
2019-02-21 11:22:16 +0100

    Always return a fresh token #9, #25

--- src/main/scala/webmodelica/controllers/JwtFilter.scala
+++ src/main/scala/webmodelica/controllers/JwtFilter.scala
@@ -7,13 +7,26 @@ import com.twitter.finatra.http.response.ResponseBuilder
 import com.twitter.util.Future
 import webmodelica.constants
 import webmodelica.services.TokenGenerator
+import webmodelica.stores.UserStore
+import webmodelica.models.{User, errors}
 
-class JwtFilter@Inject()(gen:TokenGenerator) extends SimpleFilter[http.Request, http.Response]
+
+class JwtFilter@Inject()(gen:TokenGenerator, store:UserStore) extends SimpleFilter[http.Request, http.Response]
   with com.twitter.inject.Logging {
 
   override def apply(request: Request, service: Service[Request, Response]): Future[Response] = {
     request.headerMap.get(constants.authorizationHeader) match {
-      case Some(token) if gen.isValid(token) => service(request)
+      case Some(token) if gen.isValid(token) =>
+        val userF = gen.decode(token).flatMap(t => store.findBy(t.username))
+        val respF = service(request)
+        for {
+          response <- respF
+          user <- userF.flatMap(errors.notFoundExc("web-token contains invalid user informations!"))
+          token = gen.newToken(user)
+        } yield {
+          response.authorization = token
+          response
+        }
       case _ =>
         warn(s"provided token invalid!")
         val res = Response()
