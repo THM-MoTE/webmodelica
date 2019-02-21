c17518261f34541d88a7644aadae5edad651d37b
N. Justus <nicola.justus@mni.thm.de>
2019-02-21 14:16:17 +0100

    add release script ..

---
+++ scripts/release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+versionFile="./project/version.txt"
+backendImage="thmmote/webmodelica"
+frontendImage="thmmote/webmodelica-ui"
+version=$1
+
+echo "==> Start releasing: $version"
+
+echo "==> Writing into $versionFile"
+echo $version > $versionFile
+
+echo "==> Compiling backend.."
+sbt ";clean;compile;universal:packageXzTarball"
+
+echo "==> Building frontend.."
+cd ui && npm run build
+
+echo "==> Tagging.."
+git commit $versionFile -m ':up: version '$version
+git tag -a v$version -m 'Version '$version
+
+echo "==> Generating backend image.."
+docker build -t $backendImage:$version .
+
+echo "==> Generating frontend image.."
+docker build -t $frontendImage:$version ./ui
+
+echo "==> pushing to remotes.."
+git push origin --tags
+docker push $backendImage:$version
+docker push $frontendImage:$version
