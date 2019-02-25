9336cb24ce4561b6678d0e0de39a045cb525fef2
N. Justus <nicola.justus@mni.thm.de>
2019-02-25 14:53:47 +0100

    extend Modelica Tokenizer #30

--- ui/src/models/modelica-syntax.ts
+++ ui/src/models/modelica-syntax.ts
@@ -1,13 +1,72 @@
-// Difficulty: "Easy"
-// Language definition for Java
 let ModelicaTokenizer = {
+  keywords: [
+    "algorithm", "and", "annotation", "assert", "block", "break", "class", "connect", "connector", "constant", "constrainedby", "der", "discrete", "each",
+    "else", "elseif", "elsewhen", "encapsulated", "end", "enumeration", "equation", "expandable", "extends", "external", "false", "final", "flow", "for",
+    "function", "if", "import", "impure", "in", "initial", "inner", "input", "loop", "model", "not", "operator", "or", "outer", "output", "package", "parameter",
+    "partial", "protected", "public", "pure", "record", "redeclare", "replaceable", "return", "stream", "then", "true", "type", "when", "while", "within"
+  ],
+  typeKeywords: [
+    'Real', 'Boolean', 'Integer', 'String'
+  ],
+
+  operators: [
+    '=', '>', '<', '!', '~', '?', ':', '==', '<=', '>=', '!=',
+    '&&', '||', '++', '--', '+', '-', '*', '/', '&', '|', '^', '%',
+    '<<', '>>', '>>>', '+=', '-=', '*=', '/=', '&=', '|=', '^=',
+    '%=', '<<=', '>>=', '>>>='
+  ],
+
+  // we include these common regular expressions
+  symbols: /[=><!~?:&|+\-*\/\^%]+/,
+
+  // C# style strings
+  escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+
   tokenizer: {
     root: [
-      [/algorithm/, "keyword"],
-      [/model/, "keyword"],
-      [/function/, "keyword"],
-      [/end/, "keyword"],
-    ]
+      [/[a-z_$][\w$]*/, {
+        cases: {
+          '@keywords': { token: 'keyword.$0' },
+          '@default': 'identifier'
+        }
+      }],
+      // whitespace
+      { include: '@whitespace' },
+
+      // delimiters and operators
+      [/[{}()\[\]]/, '@brackets'],
+      // numbers
+      [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+      [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+      [/\d+/, 'number'],
+
+      // strings
+      [/"([^"\\]|\\.)*$/, 'string.invalid'],  // non-teminated string
+      [/"/, { token: 'string.quote', bracket: '@open', next: '@string' }],
+
+      // characters
+      [/'[^\\']'/, 'string'],
+      [/(')(@escapes)(')/, ['string', 'string.escape', 'string']],
+      [/'/, 'string.invalid']
+
+    ],
+    comment: [
+      [/[^\/*]+/, 'comment'],
+      [/\/\*/, 'comment', '@push'],    // nested comment
+      ["\\*/", 'comment', '@pop'],
+      [/[\/*]/, 'comment']
+    ],
+    string: [
+      [/[^\\"]+/, 'string'],
+      [/@escapes/, 'string.escape'],
+      [/\\./, 'string.escape.invalid'],
+      [/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }]
+    ],
+    whitespace: [
+      [/[ \t\r\n]+/, 'white'],
+      [/\/\*/, 'comment', '@comment'],
+      [/\/\/.*$/, 'comment'],
+    ],
   }
 }
 
