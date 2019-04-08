52d10e4da6bedc75458077364c7b779232582edb
N. Justus <nicola.justus@mni.thm.de>
2019-04-08 14:27:18 +0200

    add list of simulation options

--- ui/src/models/index.ts
+++ ui/src/models/index.ts
@@ -4,6 +4,7 @@ export * from './token'
 export * from './state'
 export * from './modelica-syntax'
 export * from './compiler-error'
+import R from 'ramda'
 
 export interface SimulationResult {
   modelName: string
@@ -19,3 +20,23 @@ export interface TableFormat {
   data: number[][],
   header: string[]
 }
+
+export interface SimulationOptionSuggestions {
+  key:string,
+  values?: string[]
+}
+
+function simSugestion(k: string, ...values:string[]): SimulationOptionSuggestions {
+  return {
+    key: k,
+    values: (R.isEmpty(values)) ? undefined : values
+  }
+}
+
+export const availableSimulationOptions: SimulationOptionSuggestions[] = [
+  simSugestion("startTime"),
+  simSugestion("stopTime"),
+  simSugestion("numberOfIntervals"),
+  simSugestion("method", "dassl", "euler", "ida", "heun"),
+  simSugestion("tolerance")
+]
