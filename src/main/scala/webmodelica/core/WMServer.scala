9a327ce7e16922a206224baa942a873db17ba0d7
N. Justus <nicola.justus@mni.thm.de>
2019-04-12 11:05:33 +0200

    add twitter-StatsFilter

--- src/main/scala/webmodelica/core/WMServer.scala
+++ src/main/scala/webmodelica/core/WMServer.scala
@@ -1,9 +1,9 @@
 package webmodelica.core
 
+import com.twitter.inject.logging.MDCInitializer
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finagle.http.{Request, Response}
-
-import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
+import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter, StatsFilter}
 import com.twitter.finatra.http.routing.HttpRouter
 import webmodelica._
 
@@ -11,12 +11,16 @@ object WMServerMain extends WMServer
 
 class WMServer extends HttpServer {
 
+  MDCInitializer.init()
+
   override def jacksonModule = JsonConfigModule
   override val modules = Seq(AppModule)
 
   override def configureHttp(router: HttpRouter) {
     router
       .filter[LoggingMDCFilter[Request, Response]]
+      .filter[TraceIdMDCFilter[Request, Response]]
+      .filter[StatsFilter[Request]]
       .filter[CommonFilters]
       .add[controllers.ProjectController]
       .add[controllers.SessionController]
