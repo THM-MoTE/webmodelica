01cb1664cdeb3c3f7aa3df3fdc49632d4e182084
N. Justus <nicola.justus@mni.thm.de>
2019-04-15 10:27:48 +0200

    move simulation models into 'simulation.ts'

--- ui/src/models/index.ts
+++ ui/src/models/index.ts
@@ -4,39 +4,4 @@ export * from './token'
 export * from './state'
 export * from './modelica-syntax'
 export * from './compiler-error'
-import * as R from 'ramda'
-
-export interface SimulationResult {
-  modelName: string
-  variables: { [name: string]: number[] }
-}
-export interface SimulateRequest {
-  modelName: string
-  options: { [key: string]: string | number }
-}
-
-export interface TableFormat {
-  modelName: string,
-  data: number[][],
-  header: string[]
-}
-
-export interface SimulationOptionSuggestions {
-  key:string,
-  values?: string[]
-}
-
-function simSugestion(k: string, ...values:string[]): SimulationOptionSuggestions {
-  return {
-    key: k,
-    values: (R.isEmpty(values)) ? undefined : values
-  }
-}
-
-export const availableSimulationOptions: SimulationOptionSuggestions[] = [
-  simSugestion("startTime"),
-  simSugestion("stopTime"),
-  simSugestion("numberOfIntervals"),
-  simSugestion("method", "dassl", "euler", "ida", "heun"),
-  simSugestion("tolerance")
-]
+export * from './simulation'
