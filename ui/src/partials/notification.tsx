557958e2bab69b9300b72bca460a70a3f048088a
N. Justus <nicola.justus@mni.thm.de>
2019-05-14 10:24:46 +0200

    use Alerts not Toasts for notifications #62

--- ui/src/partials/notification.tsx
+++ ui/src/partials/notification.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
-import { Notification} from '../models/index'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { Alert } from 'react-bootstrap'
+
+import { Notification, NotificationType } from '../models/index'
+import { removeNotifications, Action } from '../redux/index';
 
 interface Props {
   notification: Notification
+  removeNotifications(n:Notification[]):void
 }
 
-export class NotificationComponent extends React.Component<Props, any> {
+export class NotificationComponentCon extends React.Component<Props, any> {
 
-  componentDidMount() {
-
+  private variant(): 'info'|'warning'|'danger' {
+    switch(this.props.notification.type) {
+      case NotificationType.Info: return 'info'
+      case NotificationType.Warning: return 'warning'
+      case NotificationType.Error: return 'danger'
+    }
   }
+
+  private removeNotification():void {
+    this.props.removeNotifications([this.props.notification])
+  }
+
   render() {
     return (
-      <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
-        <div className="toast-header">
-          <img src="..." className="rounded mr-2" alt="..." />
-            <strong className="mr-auto">Webmodelica</strong>
-            <small>11 mins ago</small>
-            <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div className="toast-body">
-            {this.props.notification.message}
-        </div>
-      </div>
+      <Alert variant={this.variant()} dismissible onClose={this.removeNotification.bind(this)}>
+        {this.props.notification.message}
+      </Alert>
     )
   }
 }
+
+function dispatchToProps(dispatch: (a: Action) => any) {
+  return bindActionCreators({ removeNotifications }, dispatch)
+}
+
+const NotificationComponent = connect(null, dispatchToProps)(NotificationComponentCon)
+export default NotificationComponent
