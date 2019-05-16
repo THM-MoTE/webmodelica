852a2b5277f6dcfc02c8871de3e27a00ea427185
N. Justus <nicola.justus@mni.thm.de>
2019-05-16 16:42:02 +0200

    refactor login-form into separate component

--- ui/src/components/landing.tsx
+++ ui/src/components/landing.tsx
@@ -11,59 +11,13 @@ import { renderErrors } from '../partials/errors'
 import { connect } from 'react-redux'
 import * as R from 'ramda'
 import { userIsAuthenticated } from '../models';
+import LoginComponent from '../partials/login-component';
 
 class LandingCon extends Component<any, any> {
-  private username: string = ''
-  private password: string = ''
-  private api: ApiClient
-
-  constructor(props: any) {
-    super(props)
-    this.api = this.props.api
-    this.state = { errors: [] }
-  }
-
-  componentDidMount() {
-    if (userIsAuthenticated(this.props.authentication))
-      this.props.history.push("/projects")
-  }
-
-  private handleSubmit(ev: any) {
-    const props = this.props
-    const username = this.username
-    const pw = this.password
-    this.api.login(username, pw)
-      .then(() => props.history.push('/projects'))
-      .catch(err => {
-        this.setState({ errors: [err] })
-      })
-    ev.preventDefault()
-  }
-
   render() {
-    const usernameChanged = (ev: any) => this.username = ev.target.value
-    const passwordChanged = (ev: any) => this.password = ev.target.value
     return (<WmContainer title="">
       <div className="row align-items-center">
-        <div className="card sm-10 mx-auto">
-          <h5 className="card-title">Login</h5>
-          <div className="card-body">
-            <Form  onSubmit={this.handleSubmit.bind(this)}>
-              <Form.Group controlId="formUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control required placeholder="Enter username" onChange={usernameChanged} />
-              </Form.Group>
-              <Form.Group controlId="formPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control required type="password" placeholder="Password" onChange={passwordChanged} />
-              </Form.Group>
-              {renderErrors(this.state.errors)}
-              <Button variant="primary" type="submit">
-                <Octicon name="sign-in" /> Submit
-              </Button>
-            </Form>
-          </div>
-        </div>
+        <LoginComponent api={this.props.api} history={this.props.history} />
       </div>
     </WmContainer>)
   }
---
+++ ui/src/partials/login-component.tsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { WmContainer } from '../partials/container'
+import { Button, Form, Alert } from 'react-bootstrap'
+//@ts-ignore
+import Octicon from 'react-octicon'
+import { ApiClient } from '../services/api-client'
+import { Redirect } from 'react-router'
+import { defaultMapDispatchToProps, mapAuthenticationToProps } from '../redux'
+import { Action } from '../redux/index'
+import { renderErrors } from '../partials/errors'
+import { connect } from 'react-redux'
+import * as R from 'ramda'
+import { userIsAuthenticated, UserAuth } from '../models/index';
+
+interface Props {
+  api: ApiClient
+  history: any
+  authentication?: UserAuth
+}
+
+class LoginComponentCon extends React.Component<Props, any> {
+  private username: string = ''
+  private password: string = ''
+
+  constructor(p:Props) {
+    super(p)
+    this.state = { errors: [] }
+  }
+
+  private handleSubmit(ev: any) {
+    const props = this.props
+    const username = this.username
+    const pw = this.password
+    props.api.login(username, pw)
+      .then(() => props.history.push('/projects'))
+      .catch(err => {
+        this.setState({ errors: [err] })
+      })
+    ev.preventDefault()
+  }
+
+  componentDidMount() {
+    if (userIsAuthenticated(this.props.authentication))
+      this.props.history.push("/projects")
+  }
+
+  render() {
+    const usernameChanged = (ev: any) => this.username = ev.target.value
+    const passwordChanged = (ev: any) => this.password = ev.target.value
+    return (
+      <div className="card sm-10 mx-auto">
+        <h5 className="card-title">Login</h5>
+        <div className="card-body">
+          <Form onSubmit={this.handleSubmit.bind(this)}>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control required placeholder="Enter username" onChange={usernameChanged} />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control required type="password" placeholder="Password" onChange={passwordChanged} />
+            </Form.Group>
+            {renderErrors(this.state.errors)}
+            <Button variant="primary" type="submit">
+              <Octicon name="sign-in" /> Submit
+              </Button>
+          </Form>
+        </div>
+      </div>
+    )
+  }
+}
+
+const LoginComponent = connect(mapAuthenticationToProps, defaultMapDispatchToProps)(LoginComponentCon)
+export default LoginComponent
