0b26c3ddb0ac83046f4529c841d6aca653c3cf3a
N. Justus <nicola.justus@mni.thm.de>
2019-05-16 13:52:54 +0200

    add factory to combine 2 TokenValidators

--- src/main/scala/webmodelica/services/TokenValidator.scala
+++ src/main/scala/webmodelica/services/TokenValidator.scala
@@ -1,8 +1,21 @@
 package webmodelica.services
 
 import com.twitter.util.Future
+import pdi.jwt.exceptions.{JwtException, JwtValidationException}
 
 trait TokenValidator {
   def decode(token:String): Future[UserToken]
   def isValid(token:String): Boolean
+}
+
+object TokenValidator {
+  def combine(a:TokenValidator, b:TokenValidator): TokenValidator = new TokenValidator {
+    override def decode(token: String): Future[UserToken] =
+      a.decode(token).rescue {
+        case _:JwtException => b.decode(token)
+      }
+    override def isValid(token: String): Boolean = a.isValid(token) || b.isValid(token)
+
+    override def toString: String = s"CombinedValidator($a, $b)"
+  }
 }
