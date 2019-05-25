0b94ecfa82ce7ff25a55ced5d5c4bda3170d4077
N. Justus <nicola.justus@mni.thm.de>
2019-05-25 11:43:44 +0200

    return <Redirect> from login-component if user is logged in #72

--- ui/src/partials/login-component.tsx
+++ ui/src/partials/login-component.tsx
@@ -54,8 +54,6 @@ class LoginComponentCon extends React.Component<Props, State> {
       console.log("updating token using authentication cookie !")
       this.props.updateToken(cookies.Authentication)
     }
-    if (userIsAuthenticated(this.props.authentication))
-      this.props.history.push("/projects")
 
     this.props.api.getAuthenticationProviders().then(providers => this.setState({providers}))
   }
@@ -63,35 +61,39 @@ class LoginComponentCon extends React.Component<Props, State> {
   render() {
     const usernameChanged = (ev: any) => this.username = ev.target.value
     const passwordChanged = (ev: any) => this.password = ev.target.value
-    return (
-      <div className="card sm-10 mx-auto">
-        <h5 className="card-title">Login</h5>
-        <div className="card-body">
-          <Form onSubmit={this.handleSubmit.bind(this)}>
-            <Form.Group controlId="formUsername">
-              <Form.Label>Username</Form.Label>
-              <Form.Control required placeholder="Enter username" onChange={usernameChanged} disabled/>
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control required type="password" placeholder="Password" onChange={passwordChanged} disabled/>
-            </Form.Group>
-            {renderErrors(this.state.errors)}
-            <ButtonGroup>
-              {this.state.providers.map(p =>
-                (<Button variant="secondary" href={p.uri} key={p.name}>
-                  {p.icon && (<Octicon name={p.icon}/>)}&nbsp;
-                  {p.name}
-                </Button>)
-              )}
-              <Button variant="primary" type="submit" disabled>
-                <Octicon name="sign-in" /> Submit
-              </Button>
-            </ButtonGroup>
-          </Form>
+    if (userIsAuthenticated(this.props.authentication)) {
+      return (<Redirect to="/projects" />)
+    } else {
+      return (
+        <div className="card sm-10 mx-auto">
+          <h5 className="card-title">Login</h5>
+          <div className="card-body">
+            <Form onSubmit={this.handleSubmit.bind(this)}>
+              <Form.Group controlId="formUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control required placeholder="Enter username" onChange={usernameChanged} disabled/>
+              </Form.Group>
+              <Form.Group controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control required type="password" placeholder="Password" onChange={passwordChanged} disabled/>
+              </Form.Group>
+              {renderErrors(this.state.errors)}
+              <ButtonGroup>
+                {this.state.providers.map(p =>
+                  (<Button variant="secondary" href={p.uri} key={p.name}>
+                    {p.icon && (<Octicon name={p.icon}/>)}&nbsp;
+                    {p.name}
+                  </Button>)
+                )}
+                <Button variant="primary" type="submit" disabled>
+                  <Octicon name="sign-in" /> Submit
+                </Button>
+              </ButtonGroup>
+            </Form>
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
