d7b3605b6ef4f5a7800254e87d930ba6d08ac64b
N. Justus <nicola.justus@mni.thm.de>
2019-06-03 10:03:00 +0200

    add withOverlay to display overlay as long as the given promise runs

--- ui/src/partials/loading-overlay.tsx
+++ ui/src/partials/loading-overlay.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import { BackgroundJobInfo, AppState } from '../models/index';
+import { Action } from '../redux/index';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 interface Props {
   msg?: string
   display?: boolean
 }
 
-export class LoadingOverlay extends React.Component<Props,any> {
+class LoadingOverlayCon extends React.Component<Props,any> {
   render() {
     const displayStr = (this.props.display) ? 'block' : 'none'
     return (
@@ -22,3 +26,19 @@ export class LoadingOverlay extends React.Component<Props,any> {
     )
   }
 }
+
+export function withOverlay<A>(setBgInfo: (running: boolean, msg: string | undefined) => Action, msg?:string): (p:Promise<A>) => Promise<A> {
+  return (p:Promise<A>) => {
+    setBgInfo(true, msg)
+    return p.finally(() => setBgInfo(false, undefined))
+  }
+}
+
+function mapToProps(state: AppState) {
+  return {
+    msg: state.jobInfo.message,
+    display: state.jobInfo.running
+  }
+}
+
+export const LoadingOverlay = connect(mapToProps, null)(LoadingOverlayCon)
