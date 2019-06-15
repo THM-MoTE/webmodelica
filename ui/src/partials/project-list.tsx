e59ede20acabb737b2af4e71b4aa54b172e8261f
N. Justus <nicola.justus@mni.thm.de>
2019-06-15 12:13:04 +0200

    fix clicking on project link
    If the user is owner: create a session, preview the project otherwise.

--- ui/src/partials/project-list.tsx
+++ ui/src/partials/project-list.tsx
@@ -23,35 +23,19 @@ export class ProjectList extends React.Component<Props, any> {
     //if the current user is project owner: create a session, open preview otherwise
     const currentUserIsOwner = p.owner === this.props.username
     const link = (currentUserIsOwner) ? "#" + p.id : `#${p.id}/preview`
-    const onClick = (currentUserIsOwner) ? (ev: any) => this.props.newSession(ev, p) : (ev: any) => this.props.previewProject(ev, p)
-    // return (<ListGroup.Item key={p.id}>
-    //   <Row className="editor-row">
-    //     <Col>
-    //       <Button variant="link" href={link} onClick={onClick}>
-    //         <Octicon name="key" className={projectIsPublic(p) ? "text-success" : "text-danger"} />
-    //         &nbsp;&nbsp;<Octicon name="repo" />
-    //         &nbsp;{p.owner} - {p.name}
-    //       </Button>
-    //     </Col>
-    //     <Col>
-    //       <ButtonGroup className="float-right">
-    //         <Button variant="outline-info" href={`#${p.id}/preview`} onClick={(ev: any) => this.props.previewProject(ev, p)}><Octicon name="device-desktop" /></Button>
-    //         <Button variant="outline-primary" onClick={() => this.props.updateVisibility(p)}><Octicon name="key" /></Button>
-    //         <Button variant="outline-danger" onClick={() => this.props.deleteProject(p)}><Octicon name="flame" /></Button>
-    //       </ButtonGroup>
-    //     </Col>
-    //   </Row>
-    // </ListGroup.Item>)
-
+    const onClick = R.compose(() => false , (currentUserIsOwner) ? (ev: any) => this.props.newSession(ev, p) : (ev: any) => this.props.previewProject(ev, p))
     return (
-      <Media as="li">
+      <Media as="li" key={p.id}>
         <a style={{width: 64, height: 64 }} className="mr-3 btn">
           <Octicon name='repo' mega/>
         </a>
         <Media.Body>
           <Row><Col>
-            <a href={'#'+p.name} onClick={(ev:any) => {this.props.newSession(ev, p); return undefined}}>{p.name}</a>
-            <h6>{p.owner}</h6>
+            <a href={link} onClick={onClick}>{p.name}</a>
+            <h6>
+              <Octicon name='person' /> {p.owner} &nbsp;&nbsp;
+              <Octicon name="key" className={projectIsPublic(p) ? "text-success" : "text-danger"} />
+            </h6>
           </Col>
            <Col><ButtonGroup size="lg" className="float-right">
              <Button variant="outline-info" href={`#${p.id}/preview`} onClick={(ev: any) => this.props.previewProject(ev, p)}><Octicon name="device-desktop" /></Button>
@@ -64,21 +48,6 @@ export class ProjectList extends React.Component<Props, any> {
     )
   }
 
-  // render() {
-  //   return (
-  //     <Card>
-  //       <Card.Header>{this.props.title}</Card.Header>
-  //       <ListGroup variant="flush">
-  //         { this.props.projects.map(p => this.renderProjectLine(p)) }
-  //         { this.props.projects.length<=0 &&
-  //           (<ListGroup.Item key="no-proj-ids">
-  //             <p>There are no projects.</p>
-  //           </ListGroup.Item>)
-  //         }
-  //       </ListGroup>
-  //     </Card>
-  //   )
-  // }
   render() {
     return (
       <ul className="list-unstyled">
