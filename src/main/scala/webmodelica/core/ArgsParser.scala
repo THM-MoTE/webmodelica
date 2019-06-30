679a84e461b31c303ab9b475060e56699849f30c
N. Justus <nicola.justus@mni.thm.de>
2019-06-30 18:05:15 +0200

    fix default environment

--- src/main/scala/webmodelica/core/ArgsParser.scala
+++ src/main/scala/webmodelica/core/ArgsParser.scala
@@ -6,7 +6,7 @@ class ArgsParser(args:Seq[String]) extends ScallopConf(args) {
   version(s"${buildinfo.BuildInfo.name} ${buildinfo.BuildInfo.version} ${buildinfo.BuildInfo.copyright}")
   val interface = opt[String]("interface", default=Some("localhost"), descr="the binding interface, defaults to `localhost`")
   val port = opt[Int]("port", default=Some(8888), descr="the binding port, defaults to `8888`")
-  val env = opt[String]("environment", default=Some("dev"), descr="the environment, defaults to `dev`")
+  val env = opt[String]("environment", default=Some("development"), descr="the environment, defaults to `development`")
   val configFile = opt[java.io.File]("configFile", descr="the config file to use")
   validateFileExists(configFile)
   validateFileIsFile(configFile)
