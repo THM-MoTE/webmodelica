3743db83cf8e204e4bf4fd62035d775a7fc375e5
N. Justus <nicola.justus@mni.thm.de>
2019-07-06 12:32:15 +0200

    add additional circe encoders

--- src/main/scala/webmodelica/controllers/InfoController.scala
+++ src/main/scala/webmodelica/controllers/InfoController.scala
@@ -12,7 +12,9 @@ import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
 import com.google.inject.Inject
 import webmodelica.models.config._
+import io.circe.generic.JsonCodec
 
+@JsonCodec
 case class Infos(
   appName:String,
   version:String,
@@ -21,22 +23,23 @@ case class Infos(
   licenseUri:String,
   commitHash:String,
 )
+object Infos {
+  import buildinfo.BuildInfo
+
+  def apply():Infos = Infos(
+    BuildInfo.name,
+    BuildInfo.version,
+    BuildInfo.copyright,
+    BuildInfo.license,
+    BuildInfo.licenseUri,
+    BuildInfo.commit
+  )
+}
 
 class InfoController @Inject()(config: WMConfig, prefix:webmodelica.ApiPrefix)
     extends Controller {
 
-  import buildinfo.BuildInfo
-
   prefix(prefix.p) {
-    get("/info") { _:Request =>
-      Infos(
-        BuildInfo.name,
-        BuildInfo.version,
-        BuildInfo.copyright,
-        BuildInfo.license,
-        BuildInfo.licenseUri,
-        BuildInfo.commit
-      )
-    }
+    get("/info") { _:Request => Infos() }
   }
 }
