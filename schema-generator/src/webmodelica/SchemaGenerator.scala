40901f631026a73aac75c6251964a0847333fdbe
N. Justus <nicola.justus@mni.thm.de>
2019-07-06 12:32:24 +0200

    [examples.raml] generate bunch of examples

--- schema-generator/src/webmodelica/SchemaGenerator.scala
+++ schema-generator/src/webmodelica/SchemaGenerator.scala
@@ -1,8 +1,12 @@
 package webmodelica
 
 import webmodelica.models._
-import java.nio.file.Path
+import java.nio.file.{
+  Path, Paths
+}
 
+import io.circe
+import io.circe.syntax._
 import com.sksamuel.avro4s._
 import org.apache.avro._
 import org.slf4j.LoggerFactory
@@ -22,6 +26,12 @@ trait Schemas {
   }
 }
 
+case class TpeWrapper[+T](value:T)(implicit tag:scala.reflect.ClassTag[T], encoder:circe.Encoder[T]) {
+  def json:String = value.asJson.toString
+  def fullName:String = tag.runtimeClass.getCanonicalName
+  def name:String = tag.runtimeClass.getSimpleName
+}
+
 object SchemaGenerator
     extends App
     with Schemas {
@@ -29,7 +39,9 @@ object SchemaGenerator
   log.info(s"schema generator started..")
   val apiDirectory = File("./doc/api")
   val schemaDirectory = apiDirectory / "schemas"
+  val exampleDirectory = apiDirectory / "examples"
   schemaDirectory.createDirectoryIfNotExists()
+  exampleDirectory.createDirectoryIfNotExists()
 
   def writeToFile(schema:Schema): File = {
     val file = schemaDirectory / (schema.getFullName+".json")
@@ -51,10 +63,29 @@ object SchemaGenerator
     file
   }
 
+  def generateExample[T](model:TpeWrapper[T]): File = {
+    val file = exampleDirectory / (model.fullName+"-example.json")
+    log.info(s"writing example for ${model.name} -> ${file}")
+    file.overwrite(model.json)
+  }
+  def generateExampleRaml[T](examples:Seq[(TpeWrapper[T], File)]): File = {
+    val file = apiDirectory / "examples.raml"
+    val line = for((model, exampleFile) <- examples) yield {
+      val path = apiDirectory relativize exampleFile
+      s"${model.name}: !include ${path}"
+    }
+    log.info(s"generating $file")
+    val content = line.mkString("\n")
+    log.debug(s"$file content: $content")
+    file.overwrite(content)
+    file
+  }
+
   val schemas = Seq(
     AvroSchema[Infos],
     AvroSchema[AuthUser],
     AvroSchema[ModelicaFile],
+    AvroSchema[ModelicaPath],
     AvroSchema[ProjectRequest],
     AvroSchema[JSProject],
     AvroSchema[JSSession],
@@ -62,6 +93,27 @@ object SchemaGenerator
     AvroSchema[SimulationResult],
   )
 
+  log.info("generating types..")
   val files = schemas.map(schema => (schema, writeToFile(schema)))
   generateRaml(files)
+
+  log.info("generating examples..")
+  import _root_.io.circe._, _root_.io.circe.generic.semiauto._
+  val user = AuthUser("tim", Some("tim@xample.org"), Some("Tim"), Some("Mueller"))
+  val projRequest = ProjectRequest(user.username, "awesomeProject", Request())
+  val project = Project(projRequest)
+  val file = ModelicaFile(Paths.get("a/b/fac.mo"), "model fac\nend fac;")
+  val path = ModelicaPath(file.relativePath)
+  val models:Seq[TpeWrapper[Any]] = Seq(
+    TpeWrapper(Infos()),
+    TpeWrapper(user),
+    TpeWrapper(projRequest),
+    TpeWrapper(JSProject(project)),
+    TpeWrapper(JSSession(Session(project), List(path))),
+    TpeWrapper(path),
+    TpeWrapper(file),
+  )
+
+  val exampleFiles = models.map(model => (model, generateExample(model)))
+  generateExampleRaml(exampleFiles)
 }
