c0126bc58ce3b15f03865e01c1094c5f878f70e9
N. Justus <nicola.justus@mni.thm.de>
2019-08-05 15:16:43 +0200

    enable typing custom simulation options #107

--- ui/src/partials/simulation-options.tsx
+++ ui/src/partials/simulation-options.tsx
@@ -7,7 +7,7 @@ import {
   updateOption,
   deleteOption
 } from '../redux/index'
-import { Row, Col, Button, ButtonGroup, Form } from 'react-bootstrap'
+import { Row, Col, Button, ButtonGroup, Form, InputGroup } from 'react-bootstrap'
 //@ts-ignore
 import Octicon from 'react-octicon'
 import { AppState, Session, TableFormat, SimulateRequest, SimulationOption, availableSimulationOptions, simulationValuesFor } from '../models/index'
@@ -15,6 +15,13 @@ import { ApiClient } from '../services/api-client'
 import * as R from 'ramda';
 
 type Option = SimulationOption
+
+//indicates if we should render a select box or input field for the option's name
+enum BoxType {
+  Select,
+  Input
+}
+function switchBoxType(tpe:BoxType) { return (tpe===BoxType.Input) ? BoxType.Select : BoxType.Input }
 
 interface Props {
   options: Option[]
@@ -24,12 +31,13 @@ interface Props {
   simulateClicked(ev:any):void
 }
 interface State {
+  optionBoxType:BoxType[]
 }
 
 class SimulationOptionsCon extends React.Component<Props, State> {
   constructor(p:Props) {
     super(p)
-    this.state = {}
+    this.state = {optionBoxType: R.repeat(BoxType.Select, p.options.length)}
   }
 
   private updateName(idx:number, value:string): void {
@@ -40,21 +48,39 @@ class SimulationOptionsCon extends React.Component<Props, State> {
   }
 
   private addOptionField(): void {
+    const dublicate = this.state.optionBoxType.copyWithin(0,0)
+    dublicate.push(BoxType.Select)
+    this.setState({optionBoxType: dublicate})
     this.props.addOption({ name: "", value: "" })
   }
-  private deleteOptionField = this.props.deleteOption
+  private deleteOptionField(idx:number) {
+    this.setState({optionBoxType: this.state.optionBoxType.filter((v, i) => i!==idx)})
+    this.props.deleteOption(idx)
+  }
 
+  private isTextBoxType = (idx:number) => (idx < this.state.optionBoxType.length) && this.state.optionBoxType[idx] === BoxType.Input
   render() {
+    //update the option's name input type
+    const switchBoxTypeClicked = (idx:number) => {
+      //search for idx and replace the value
+      const optionBoxType = this.state.optionBoxType.map((v, i) => (i===idx) ? switchBoxType(v) : v)
+      this.setState({optionBoxType})
+    }
     return (<>
       {this.props.options.map((opt, idx) =>  {
         const values = simulationValuesFor(opt.name)
         return (
           <Form.Row key={idx}>
-            <Col sm={5}>
-              <Form.Control as="select" placeholder="name" value={opt.name} onChange={(ev:any) => this.updateName(idx, ev.target.value)}>
-                {availableSimulationOptions.map(o => (<option key={o.key}>{o.key}</option>))}
-              </Form.Control>
-            </Col>
+            <InputGroup as={Col} sm={5}>
+              <InputGroup.Prepend><Button variant="secondary" onClick={() => switchBoxTypeClicked(idx)}><Octicon name={(this.isTextBoxType(idx)) ? 'three-bars' : 'pencil'} /></Button></InputGroup.Prepend>
+              { this.isTextBoxType(idx) && (
+                <Form.Control placeholder="name" value={opt.name} onChange={(ev: any) => this.updateName(idx, ev.target.value)} />
+              )}
+              { !this.isTextBoxType(idx) && (                
+                <Form.Control as='select' placeholder="name" value={opt.name} onChange={(ev:any) => this.updateName(idx, ev.target.value)}>
+                  {availableSimulationOptions.map(o => (<option key={o.key}>{o.key}</option>))}
+                </Form.Control>)}
+            </InputGroup>
             <Col sm={6}>
             { //if there are suggestions, provide a selector
               !R.isEmpty(values) &&
