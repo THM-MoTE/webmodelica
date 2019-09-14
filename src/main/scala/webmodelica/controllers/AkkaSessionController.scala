6f43103e9f598f6a56fa95f0f715bc82c6dcf548
N. Justus <nicola.justus@mni.thm.de>
2019-09-14 17:51:43 +0200

    add start of session controller

---
+++ src/main/scala/webmodelica/controllers/AkkaSessionController.scala
@@ -0,0 +1,55 @@
+package webmodelica.controllers
+
+import com.twitter.util.{Future => TFuture}
+import akka.http.scaladsl.server._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model._
+import webmodelica.models._
+import webmodelica.models.config._
+import webmodelica.services._
+import webmodelica.stores._
+import webmodelica.conversions.futures._
+import io.scalaland.chimney.dsl._
+import java.nio.file.{
+  Paths,
+  Path
+}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import io.circe.generic.JsonCodec
+
+object AkkaSessionController {
+}
+
+class AkkaSessionController(
+  override val userStore: UserStore,
+  override val projectStore:ProjectStore,
+  override val gen: CombinedTokenValidator,
+  sessionRegistry: SessionRegistry,
+  maxSimulationData:MaxSimulationData)
+    extends AkkaUserExtractor
+    with AkkaProjectExtractor
+    with com.typesafe.scalalogging.LazyLogging
+    with de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+    with AkkaController {
+  override val routes:Route = logRequest("/sessions") {
+    (extractUser & pathPrefix("sessions")) { (user:User) =>
+      pathPrefix(Segment) { (id:String) =>
+        def service(): TFuture[SessionService] = sessionRegistry.get(id).flatMap(errors.notFoundExc(s"Can't find a session for: $id"))
+        (delete & pathEnd) {
+          complete(
+            sessionRegistry.killSession(id)
+              .map(_ => StatusCodes.NoContent)
+              .asScala
+          )
+        } ~
+        pathPrefix("files") {
+          (path("update") & post & entity(as[ModelicaFile])) { case file =>
+            val future = service().flatMap(_.update(file)).asScala
+            complete(future)
+          }
+        }
+      }
+    }
+  }
+}
--- src/main/scala/webmodelica/core/WMServer.scala
+++ src/main/scala/webmodelica/core/WMServer.scala
@@ -44,7 +44,7 @@ class WMServer extends LazyLogging
   def bootstrap(module:WebmodelicaModule): Unit = {
     import module._
     // val ctrl = AkkaController.combineAll(Seq(wire[AkkaProjectController], AkkaInfoController))
-    val ctrl = wire[AkkaProjectController] || AkkaInfoController
+    val ctrl = wire[AkkaProjectController] || wire[AkkaSessionController] || AkkaInfoController
     val routes:Route = handleExceptions(exceptionMapper) {
       pathPrefix("api"/"v1"/"webmodelica") { ctrl.routes }
     }
