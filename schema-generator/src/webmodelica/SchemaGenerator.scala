1948bc226bfef07b15ebafe6b5879195debb4bc5
N. Justus <nicola.justus@mni.thm.de>
2019-09-19 15:21:56 +0200

    fix SchemaGenerator

--- schema-generator/src/webmodelica/SchemaGenerator.scala
+++ schema-generator/src/webmodelica/SchemaGenerator.scala
@@ -11,8 +11,6 @@ import better.files.File.OpenOptions
 import cats.{Applicative, Monad}
 import com.twitter.finagle.http.Request
 import webmodelica.controllers._
-import webmodelica.controllers.ProjectController._
-import webmodelica.controllers.SessionController._
 import webmodelica.models.mope._
 import webmodelica.models.mope.requests._
 import webmodelica.models.mope.responses._
@@ -64,14 +62,14 @@ object SchemaGenerator
 
   import _root_.io.circe._, _root_.io.circe.generic.semiauto._
   val user = AuthUser("tim", Some("tim@xample.org"), Some("Tim"), Some("Mueller"))
-  val projRequest = ProjectRequest(user.username, "awesomeProject", Request())
+  val projRequest = AkkaProjectController.ProjectRequest(user.username, "awesomeProject")
   val project = Project(projRequest)
   val file = ModelicaFile(Paths.get("a/b/fac.mo"), "model fac\nend fac;")
   val path = ModelicaPath(file.relativePath)
   val models:Seq[TpeWrapper[Any]] = Seq(
     TpeWrapper(AvroSchema[Infos], Infos()),
     TpeWrapper(AvroSchema[AuthUser], user),
-    TpeWrapper(AvroSchema[ProjectRequest], projRequest),
+    TpeWrapper(AvroSchema[AkkaProjectController.ProjectRequest], projRequest),
     TpeWrapper(AvroSchema[JSProject], JSProject(project)),
     TpeWrapper(AvroSchema[JSSession], JSSession(Session(project), List(path))),
     TpeWrapper(AvroSchema[ModelicaPath], path),
